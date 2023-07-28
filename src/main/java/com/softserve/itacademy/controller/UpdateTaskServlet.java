f1311951fb02c0bcf0a513edeec90f3da7a14150
SerhiiLunov <lunevlife@gmail.com>
2023-07-29 00:00:15 +0300

    push UpdateTaskServlet + jsp

--- src/main/java/com/softserve/itacademy/controller/UpdateTaskServlet.java
+++ src/main/java/com/softserve/itacademy/controller/UpdateTaskServlet.java
@@ -1,25 +1,73 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.model.Priority;
+import com.softserve.itacademy.model.Task;
 import com.softserve.itacademy.repository.TaskRepository;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet("/edit-task?id=<task ID>")
 public class UpdateTaskServlet extends HttpServlet {
 
     private TaskRepository taskRepository;
+    private  int taskId;
 
     @Override
     public void init() {
         taskRepository = TaskRepository.getTaskRepository();
     }
+//
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+//
+//    }
+//
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+//
+//    }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         taskId = Integer.parseInt(request.getParameter("id"));
+        Task task = taskRepository.read(taskId);
+        if (task != null) {
+            request.setAttribute("task", task);
+            request.getRequestDispatcher("/WEB-INF/pages/edit-task.jsp").forward(request, response);
+        } else {
+            response.sendRedirect(request.getContextPath() + "/create-task");
+        }
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Task task = taskRepository.read(taskId);
 
+        if (task != null) {
+            String title = request.getParameter("title");
+            Priority priority = Priority.valueOf(request.getParameter("priority"));
+
+            if (title != null && priority != null) {
+                Task existingTask = taskRepository.findByTitle(title);
+
+                if (existingTask != null && existingTask.getId() != taskId) {
+                    request.setAttribute("errorMessage", "Task with a given name already exists!");
+                } else {
+                    task.setTitle(title);
+                    task.setPriority(priority);
+                    taskRepository.update(task);
+                }
+            } else {
+                request.setAttribute("errorMessage", "Please provide all the necessary details!");
+            }
+        } else {
+            response.sendRedirect(request.getContextPath() + "/create-task");
+        }
+        doGet(request, response);
     }
+
 }
+
--- src/main/webapp/WEB-INF/pages/edit-task.jsp
+++ src/main/webapp/WEB-INF/pages/edit-task.jsp
@@ -11,9 +11,58 @@
 
 </head>
 <body>
-    <%@include file="header.html"%>
+<%@include file="header.html" %>
 
+<h2>Edit existing Task</h2>
 
+<% if (request.getAttribute("errorMessage") != null) { %>
+<p><%= request.getAttribute("errorMessage") %>
+</p>
+<% } %>
+
+<form action="/edit-task" method="post">
+    <%
+        Task task = (Task) request.getAttribute("task");
+    %>
+    <table>
+        <tr>
+            <td>
+                <label for="id">id: </label>
+            </td>
+            <td>
+                <input type="text" id="id" name="id" value="<%=task.getId() %>" disabled>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="title">Title: </label>
+            </td>
+            <td>
+                <input type="text" id="title" name="title" value="<%=task.getTitle() %>">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="priority">Priority: </label>
+            </td>
+            <td>
+                <select name="priority" id="priority" value="<%=task.getPriority() %>">
+                    <option value="LOW">Low</option>
+                    <option value="MEDIUM">Medium</option>
+                    <option value="HIGH">High</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" value="Edit">
+            </td>
+            <td>
+                <input type="reset" value="Clear">
+            </td>
+        </tr>
+    </table>
+</form>
 
 </body>
 </html>
