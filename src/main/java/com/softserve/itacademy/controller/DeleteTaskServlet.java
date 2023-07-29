02a564a3ca5843f4c687b8727165247df592a182
SerhiiLunov <lunevlife@gmail.com>
2023-07-29 10:05:29 +0300

    push DeleteTaskServlet

--- src/main/java/com/softserve/itacademy/controller/DeleteTaskServlet.java
+++ src/main/java/com/softserve/itacademy/controller/DeleteTaskServlet.java
@@ -1,25 +1,29 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.model.Task;
 import com.softserve.itacademy.repository.TaskRepository;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet("/delete-task?id=<task ID>")
 public class DeleteTaskServlet extends HttpServlet {
 
     private TaskRepository taskRepository;
 
     @Override
-    public void init()  {
+    public void init() {
         taskRepository = TaskRepository.getTaskRepository();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        taskRepository.delete(Integer.parseInt(request.getParameter("id")));
+        response.sendRedirect("/tasks-list");
     }
 }
+
